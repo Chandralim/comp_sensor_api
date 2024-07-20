e986771de805f591e14d2e8540bc90b758295739
ChandraLim92 <seven7planet@yahoo.com>
2024-07-20 12:30:42 +0700

    fix port

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted'=>false,
-                'host'=>'127.0.0.1',
-                'port'=>6001,
-                'scheme'=>'http',
+                'encrypted' => false,
+                'host' => '127.0.0.1',
+                'port' => 6005,
+                'scheme' => 'http',
                 // 'useTLS' => true,
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
