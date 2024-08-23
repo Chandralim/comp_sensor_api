35f36cf2343bb5a667a4e27d74b1ca64be5433d7
ChandraLim92 <seven7planet@yahoo.com>
2024-08-23 20:51:15 +0700

    test db transaction bulk send

--- app/Http/Controllers/IOT/SensorDataRawController.php
+++ app/Http/Controllers/IOT/SensorDataRawController.php
@@ -21,7 +21,8 @@ use App\Helpers\MyLog;
 use App\Model\SensorList;
 use App\Model\SensorDataRaw;
 use Excel;
-use DB;
+use Illuminate\Support\Facades\DB;
+
 
 class SensorDataRawController extends Controller
 {
@@ -97,58 +98,74 @@ class SensorDataRawController extends Controller
 
   public function store(Request $request)
   {
-    $created_at = $request->created_at;
-    if (!$created_at) {
-      throw new MyException(["message" => "Created At Need To Fill"], 422);
-    }
+    DB::beginTransaction();
+    try {
+      $created_at = $request->created_at;
+      if (!$created_at) {
+        throw new MyException(["message" => "Created At Need To Fill"], 422);
+      }
 
-    $zone = "";
-    $pattern_utc = "/^.{10}T.{8}Z$/i";
-    $pattern_asia = "/^.{10}T.{8} 07:00$/i";
-    if (preg_match($pattern_utc, $created_at)) {
-      $zone = "UTC";
-    } elseif (preg_match($pattern_asia, $created_at)) {
-      $zone = "Jakarta";
-      $created_at = str_replace(" ", "+", $created_at);
-    } else {
-      throw new MyException(["message" => "Created At Format Not Match"], 422);
-    }
+      $zone = "";
+      $pattern_utc = "/^.{10}T.{8}Z$/i";
+      $pattern_asia = "/^.{10}T.{8} 07:00$/i";
+      if (preg_match($pattern_utc, $created_at)) {
+        $zone = "UTC";
+      } elseif (preg_match($pattern_asia, $created_at)) {
+        $zone = "Jakarta";
+        $created_at = str_replace(" ", "+", $created_at);
+      } else {
+        throw new MyException(["message" => "Created At Format Not Match"], 422);
+      }
 
-    // echo date("Y-m-d H:i:s", strtotime($created_at));
-    $datetime = new \DateTime($created_at);
-    $datetime->setTimezone(new \DateTimeZone('UTC'));
-    // echo $datetime->format('Y-m-d H:i:s');
+      // echo date("Y-m-d H:i:s", strtotime($created_at));
+      $datetime = new \DateTime($created_at);
+      $datetime->setTimezone(new \DateTimeZone('UTC'));
+      // echo $datetime->format('Y-m-d H:i:s');
 
-    $utc_millis = $datetime->getTimestamp() * 1000;
-    $return = [];
-    foreach ($request->all() as $k => $v) {
-      $pattern = "/^snsr_/i";
-      if (preg_match($pattern, $k)) {
-        $uname = preg_replace($pattern, "", $k);
-        $value = $v;
+      $utc_millis = $datetime->getTimestamp() * 1000;
+      $return = [];
+      foreach ($request->all() as $k => $v) {
+        $pattern = "/^snsr_/i";
+        if (preg_match($pattern, $k)) {
+          $uname = preg_replace($pattern, "", $k);
+          $value = $v;
 
-        $sl = SensorList::where('uname', $uname)->where("sensor_token_id", $this->sensor->id)->first();
-        if ($sl) {
-          $dt = [
-            "created_at"      => $utc_millis,
-            "sensor_list_id"  => $sl->id,
-            "value"           => $value,
-          ];
+          $sl = SensorList::where('uname', $uname)->where("sensor_token_id", $this->sensor->id)->first();
+          if ($sl) {
+            $dt = [
+              "created_at"      => $utc_millis,
+              "sensor_list_id"  => $sl->id,
+              "value"           => $value,
+            ];
 
-          if (!SensorDataRaw::where("created_at", $utc_millis)->where("sensor_list_id", $sl->id)->where("value", $value)->first())
-            SensorDataRaw::insert($dt);
+            if (!SensorDataRaw::where("created_at", $utc_millis)->where("sensor_list_id", $sl->id)->where("value", $value)->first())
+              SensorDataRaw::insert($dt);
 
-          $dt['sensor_token_id'] = $this->sensor->id;
-          array_push($return, $dt);
+            $dt['sensor_token_id'] = $this->sensor->id;
+            array_push($return, $dt);
+          }
         }
       }
+
+
+      DB::commit();
+      broadcast(new SensorDataRawReceived($return));
+      return response()->json([
+        "message" => "Proses tambah data berhasil",
+      ], 200);
+    } catch (\Exception $e) {
+      DB::rollback();
+
+      if ($e->getCode() == 1) {
+        return response()->json([
+          "message" => $e->getMessage(),
+        ], 400);
+      }
+
+      return response()->json([
+        "message" => "Proses tambah data gagal"
+      ], 400);
     }
-
-    broadcast(new SensorDataRawReceived($return));
-
-    return response()->json([
-      "message" => "Proses tambah data berhasil",
-    ], 200);
   }
 
   //     $utcMillis = 1678986600000;
