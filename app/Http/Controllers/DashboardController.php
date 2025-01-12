d135a93e4798644eedd895cbfb2eba245dc7d6e2
ChandraLim92 <seven7planet@yahoo.com>
2025-01-12 17:23:55 +0700

    fixing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -417,16 +417,11 @@ class DashboardController extends Controller
 
       $sensor_token_id = $request->sensor_token_id;
 
-      $data = SensorToken::with(['sensor_lists' => function ($q) use ($utc_date_from, $utc_date_to) {
+      $d1millis = 1000 * 60 * 60;
 
-        $q->with(['sensor_datas as first_sensor_data' => function ($q2) use ($utc_date_from) {
-          $q2->where('created_at', '<', $utc_date_from)
-            ->orderBy('created_at', 'desc')
-            ->limit(1); // Limit to 1 to get the first record
-        }]);
-
-        $q->with(['sensor_datas as filtered_sensor_datas' => function ($q2) use ($utc_date_from, $utc_date_to) {
-          $q2->where('created_at', ">=", $utc_date_from)->where('created_at', "<", $utc_date_to)->orderBy("created_at", "asc");
+      $data = SensorToken::with(['sensor_lists' => function ($q) use ($utc_date_from, $utc_date_to, $d1millis) {
+        $q->with(['sensor_datas' => function ($q2) use ($utc_date_from, $utc_date_to, $d1millis) {
+          $q2->where('created_at', ">=", $utc_date_from - $d1millis)->where('created_at', "<", $utc_date_to)->orderBy("created_at", "asc");
         }]);
       }])->find($sensor_token_id);
 
@@ -451,15 +446,17 @@ class DashboardController extends Controller
         array_push($myData, $md);
       }
 
-      $sensor_lists = $data->sensor_lists[0]->filtered_sensor_datas->toArray();
+      $sensor_lists = $data->sensor_lists->toArray();
       foreach ($sensor_lists as $k => $v) {
 
         $sensor_lists[$k]["sensor_postVal"] = [];
 
         foreach ($myData as $k1 => $v1) {
 
-          $af = array_values(array_filter($v['sensor_datas'], function ($x) use ($v1) {
-            return $x['created_at'] >= $v1['utc_from'] && $x['created_at'] < $v1['utc_to'];
+          if ($k > $k1) break;
+
+          $af = array_values(array_filter($v['sensor_datas'], function ($x) use ($v1, $d1millis) {
+            return $x['created_at'] >= ($v1['utc_from'] - $d1millis) && $x['created_at'] < ($v1['utc_to'] - $d1millis);
           }));
 
           $result = 0;
@@ -467,7 +464,7 @@ class DashboardController extends Controller
           } elseif (count($af) == 1 && $v['type'] == 'inc') {
           } elseif (count($af) == 1 && $v['type'] == 'ran') {
           } elseif ($v['type'] == 'inc') {
-            $result = $af[count($af) - 1]['value'];
+            $result = $af[0]['value'];
             // $result = ($af[count($af) - 1]['value'] - $af[0]['value']) / 2;
           } elseif ($v['type'] == 'ran') {
             $result = array_reduce($af, function ($carry, $item) {
